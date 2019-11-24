8560397e4b90a27b152f04e221df9ecd5858d955
Andrei Fedianov <andreifaa@gmail.com>
2019-11-24 16:44:33 -0500

    💾

--- lib/trivium_web/views/error_helpers.ex
+++ lib/trivium_web/views/error_helpers.ex
@@ -9,9 +9,9 @@ defmodule TriviumWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field) do
+  def error_tag(form, field, options \\ []) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error), class: "help-block")
+      content_tag(:small, translate_error(error), options || [class: "help-block"])
     end)
   end
 
